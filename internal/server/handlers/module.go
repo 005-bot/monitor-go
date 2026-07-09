b6656362f298c6ad9998877ff2a1ff8914fce725
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-07-09 10:31:15 +0700

    [server] add test endpoints

--- internal/server/handlers/module.go
+++ internal/server/handlers/module.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/005-bot/monitor-go/internal/server/handlers/monitor"
 	"github.com/005-bot/monitor-go/internal/server/handlers/parser"
 	"github.com/005-bot/monitor-go/internal/server/handlers/scraper"
 	"github.com/005-bot/monitor-go/internal/server/handlers/storage"
@@ -14,6 +15,11 @@ func Module() fx.Option {
 		"handlers",
 		logger.WithNamedLogger("handlers"),
 		fx.Provide(
+			fx.Annotate(
+				monitor.NewHandler,
+				fx.As(new(handler.Handler)),
+				fx.ResultTags(`group:"handlers"`),
+			),
 			fx.Annotate(
 				storage.NewHandler,
 				fx.As(new(handler.Handler)),
