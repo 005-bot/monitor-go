409134c0065d9f1adc8045f04a0fb582b7f6a9d1
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-07-10 18:35:32 +0700

    [config] validation

--- internal/config/module.go
+++ internal/config/module.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"context"
+
 	"github.com/005-bot/monitor-go/internal/parser/address"
 	"github.com/005-bot/monitor-go/internal/publisher"
 	"github.com/005-bot/monitor-go/internal/scheduler"
@@ -10,6 +12,7 @@ import (
 	"github.com/go-core-fx/fiberfx/openapi"
 	"github.com/go-core-fx/redisfx"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 func Module() fx.Option {
@@ -65,5 +68,16 @@ func Module() fx.Option {
 				}
 			},
 		),
+		fx.Invoke(func(lc fx.Lifecycle, cfg Config, logger *zap.Logger) {
+			lc.Append(fx.StartHook(func(_ context.Context) error {
+				if err := cfg.Validate(); err != nil {
+					logger.Error("invalid configuration", zap.Error(err))
+
+					return err
+				}
+
+				return nil
+			}))
+		}),
 	)
 }
